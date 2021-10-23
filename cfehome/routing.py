e5ba7e8ceae94b7aa5d03243625e307547795421
sachinmanandhar <sacheenmdr9@gmail.com>
2021-10-23 19:26:17 +0545

    first commit

---
+++ cfehome/routing.py
@@ -0,0 +1,15 @@
+from channels.routing import ProtocolTypeRouter,URLRouter
+from django.urls import path
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+from map_app import consumer
+
+websocket_urlPattern = [
+    path('ws/polData/',consumer.DashConsumer.as_asgi()),
+]
+application = ProtocolTypeRouter ({
+   'websocket': AuthMiddlewareStack(URLRouter(
+       websocket_urlPattern
+   ))
+})
