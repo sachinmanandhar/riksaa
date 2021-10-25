d7847d3a71b62f48c52814d58878e2105dd948b8
sachinmanandhar <sacheenmdr9@gmail.com>
2021-10-25 10:27:49 +0545

    real time data

--- manage.py
+++ manage.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
-=======
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -14,7 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auto.settings')
->>>>>>> 628e853ac584df5a4264359e160e0a0610ef8ee4
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -24,10 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-<<<<<<< HEAD
-=======
 
 
 if __name__ == '__main__':
     main()
->>>>>>> 628e853ac584df5a4264359e160e0a0610ef8ee4
